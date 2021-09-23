df7ff2dddd55596a116913fcd047aed58512c315
Kevinayad <kevinay@student.chalmers.se>
2021-09-23 15:27:38 +0200

    added get and get by id

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,12 +3,28 @@ var router = express.Router();
 var User = require('../models/user');
 router.post('/api/users', function(req, res, next) {
     var user = new User(req.body);
-             user.save(function(err, user) {
+             user.save(function(err,users) {
                 if (err) {return next(err);}
         res.status(201).json(user);
     })
     });
 
     
-    router.get('/api/users', function(req, res) {res.json({"users":user});});
+    router.get('/api/users', function(req, res, next) {User.find(function(err, users) {
+        if (err) { return next(err); }
+        res.json({"users": users});});});
+
+        router.get('/api/users/:_id', function(req, res, next) {
+            var id = req.params._id;
+            User.findById(id, function(err, users) {
+                if (err) { return next(err); }
+                if (users == null) {
+                    return res.status(404).json({"message": "user not found"});
+                }
+                res.json(users);
+            });
+        });
+        
+    
+
 module.exports = router;
