58003fb64fd37733f896840a72778121d05e9960
guserlhae <hampuser@student.chalmers.se>
2021-09-23 16:07:58 +0200

    Fixed delete and added delete by ID

--- server/controllers/reminders.js
+++ server/controllers/reminders.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Reminder = require('../models/reminder');
 
+//Post/add to database
 router.post('/api/reminders',function(req,res,next){
     var reminder = new Reminder(req.body);
     reminder.save(function(err, reminder){
@@ -11,6 +12,7 @@ router.post('/api/reminders',function(req,res,next){
         res.status(201).json(reminder);
     });
 });
+//Get all
 router.get('/api/reminders',function(req,res,next){
     Reminder.find(function(err,reminders){
         if(err){
@@ -19,6 +21,7 @@ router.get('/api/reminders',function(req,res,next){
         res.json({"reminders":reminders})
     });
 });
+//Get by ID
 router.get('/api/reminders/:_id',function(req,res,next){
     id = req.params._id;
     Reminder.findById(id,function(err,reminders){
@@ -28,13 +31,25 @@ router.get('/api/reminders/:_id',function(req,res,next){
         res.json({"reminders":reminders})
     });
 });
+//Delete all
 router.delete('/api/reminders',function(req,res,next){
-    Reminder.deleteMany(function(err,reminders){
+    Reminder.deleteMany(function(err){
         if(err){
             return next(err);
         }
-        res.status(201).json.Reminder;
-    })
+        res.status(201).json(Reminder);
+    });
 
-})
+});
+//Delete by ID
+router.delete('/api/reminders/:_id',function(req,res,next){
+    id = req.params._id;
+    Reminder.findOneAndDelete({_id:id},function(err,reminders){
+        if(err){
+            return next(err);
+        }
+        res.status(201).json(Reminder);
+    });
+    
+});
 module.exports = router;
