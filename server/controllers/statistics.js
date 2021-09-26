1ff2682e3451719ddb5fa5e94634d2b0c5205afc
guserlhae <hampuser@student.chalmers.se>
2021-09-26 18:01:18 +0200

    Statistics get function'

--- server/controllers/statistics.js
+++ server/controllers/statistics.js
@@ -7,8 +7,25 @@ router.post('/api/statistics', function(req, res, next) {
              statistic.save(function(err, statistic) {
                 if (err) {return next(err);}
         res.status(201).json(statistic);
-    })
     });
+});
+router.get('api/statistics',function(req,res,next){
+    Statistic.find(function(err,reminders){
+        if(err){
+            return next(err);
+        }
+        res.json({"statstics":statistics});
+    });
+});
+router.get('api/statistics/:_id',function(req,res,next){
+    var id = req.params._id;
+    Statistic.findById(id,function(err,statistics){
+        if(err){
+            return next(err);
+        }
+        res.json({"statistics":statistics})
+    })
+})
 
     module.exports = router;
     
