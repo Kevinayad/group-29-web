b6f01007a1c6263c0eb6432f8c8407e28c2bed55
Kevinayad <kevinay@student.chalmers.se>
2021-09-30 20:00:58 +0200

    added post function

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand">Physion</a>
+  <a class="navbar-brand">Physion Admin Page</a>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Users <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="#">Leaderboards</a>
@@ -18,7 +18,7 @@
       </li>
     </ul>
   </div>
-</nav>
+  </nav>
     <!-- Render the content of the current page view -->
     <router-view/>
   </div>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,41 +1,78 @@
 <template>
   <div class="container">
-<div class="row">
-    <div class="col-sm">
-     <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Username</label>
-    <input type="username" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Username">
-    <small id="emailHelp" class="form-text text-muted">Fill in chosen username</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Goal</label>
-    <input type="height" class="form-control" id="height" placeholder="Enter users goal">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Height</label>
-    <input type="height" class="form-control" id="height" placeholder="Enter height in cm">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Weight</label>
-    <input type="weight" class="form-control" id="weight" placeholder="Enter weight in KG">
-  </div>
-   <div class="form-group">
-    <label for="exampleFormControlSelect1">Gender</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>Female</option>
-      <option>Male</option>
-      <option>Prefer not to say</option>
-    </select>
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+    <div class="row">
+      <div class="col-sm"><h1>Users registration page</h1></div>
     </div>
-</div>
+    <div class="row">
+      <div class="col-sm">
+        <form @submit.prevent="submit">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input
+              type="username"
+              class="form-control"
+              id="username"
+              aria-describedby="emailHelp"
+              placeholder="Username"
+              required
+              v-model="name"
+            />
+            <small id="emailHelp" class="form-text text-muted"
+              >Fill in chosen username</small
+            >
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Goal</label>
+            <input
+              type="height"
+              class="form-control"
+              id="height"
+              placeholder="Enter users goal"
+              required
+              v-model="goals"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Height</label>
+            <input
+              type="height"
+              class="form-control"
+              id="height"
+              placeholder="Enter height in cm"
+              required
+              v-model="height"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Weight</label>
+            <input
+              type="weight"
+              class="form-control"
+              id="weight"
+              placeholder="Enter weight in KG"
+              required
+              v-model="weight"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Gender</label>
+            <select
+              class="form-control"
+              id="exampleFormControlSelect1"
+              required
+              v-model="gender"
+            >
+              <option>Female</option>
+              <option>Male</option>
+              <option>Prefer not to say</option>
+            </select>
+          </div>
+          <div class="submitForm">
+            <button type="text" class="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -44,20 +81,29 @@
 import { Api } from '@/Api'
 
 export default {
-  name: 'home',
   data() {
     return {
-      message: 'none'
+      name: '',
+      gender: '',
+      height: '',
+      weight: '',
+      goals: ''
     }
   },
   methods: {
-    getMessage() {
-      Api.get('/')
-        .then(response => {
-          this.message = response.data.message
+    submit() {
+      Api.post('/users', {
+        name: this.name,
+        gender: this.gender,
+        height: this.height,
+        weight: this.weight,
+        goals: this.goals
+      })
+        .then((response) => {
+          alert('User registered')
         })
-        .catch(error => {
-          this.message = error
+        .catch((error) => {
+          alert(error)
         })
     }
   }
