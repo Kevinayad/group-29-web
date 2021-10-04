658f7b97d57c91ff3c43adb763212130f398a507
guserlhae <hampuser@student.chalmers.se>
2021-10-04 15:46:05 +0200

    Minor code cleaning and automated postman testing

--- server/controllers/leadboards.js
+++ server/controllers/leadboards.js
@@ -2,20 +2,10 @@ var express = require('express');
 var router = express.Router();
 var Leadboard = require('../models/leadboard');
 
-router.post('/api/leadboards', function(req, res, next) {
-    var leadboard = new Leadboard(req.body);
-             leadboard.save(function(err, leadboard) {
-                if (err) {return next(err);}
-        res.status(201).json(leadboard);
-    })
-    });
-
-    module.exports = router;
-    
     // creating new leaderboard
     router.post('/api/leadboards', function(req, res, next) {
         var leadboard = new Leadboard(req.body);
-                 leadboard.save(function(err, leadboard) {
+                 leadboard.save(function(err, leadboards) {
                     if (err) {return next(err);}
             res.status(201).json(leadboards);
         })
