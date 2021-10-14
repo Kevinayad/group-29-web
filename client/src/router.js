e268a1508f7c170a8cf75b45243b61c39c7cd418
hampuser <hampuser@student.chalmers.se>
2021-10-14 22:06:59 +0000

    Fixing merge conflicts

--- client/src/router.js
+++ client/src/router.js
@@ -6,6 +6,7 @@ import Leaderboard from './views/Leaderboards.vue'
 import Statistic from './views/Statistics.vue'
 import EditStatistic from './views/EditStatistics.vue'
 import Reminder from './views/Reminders.vue'
+import getAllUsers from './views/getAllUsers.vue'
 
 Vue.use(Router)
 
@@ -42,6 +43,10 @@ export default new Router({
       path: '/reminders',
       name: 'reminders',
       component: Reminder
+    },{
+      path: '/getusers',
+      name: 'getusers',
+      component: getAllUsers
     }
   ]
 })
