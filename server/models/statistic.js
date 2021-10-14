a52f0cf549561f28641d8499b032eec1f7ea5925
annseri <annseri@student.chalmers.se>
2021-10-14 13:23:39 +0000

    Update statistic.js

--- server/models/statistic.js
+++ server/models/statistic.js
@@ -1,12 +1,9 @@
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var statisticSchema = new Schema ({
     type: { type: String},
-    timespan: { type: String},
-    name: { type: String},
-    goal: { type: String}
+    timespan: { type: String}
 
 });
 module.exports = mongoose.model('statistics', statisticSchema);
